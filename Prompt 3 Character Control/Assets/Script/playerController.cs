450e8ebd0703e26909ecddc7258bd909f9307759
HongkaiD <75577452+HongkaiD@users.noreply.github.com>
2021-05-11 13:02:39 -0400

    adjust camera

--- Prompt 3 Character Control/Assets/Script/playerController.cs
+++ Prompt 3 Character Control/Assets/Script/playerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class playerController : MonoBehaviour
 {
     public float moveSpeed;
     public float cameraSpeed;
